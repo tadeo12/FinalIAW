2835da64f68b3f3349a780a7d358599a49e73651
tadeo12 <tadeoova@gmail.com>
2023-01-15 18:12:06 -0300

    primer commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import NewReview from "./Components/NewReview.svelte";
+	export let name;
+</script>
+
+<main>
+	<h1>Reviews {name} </h1>
+	<NewReview>
+
+	</NewReview>
+</main>
+
+<style>
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 300;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Components/NewReview.svelte
@@ -0,0 +1,22 @@
+
+<script>
+    let filmName= "Harry Potter: Las reliquias de la muerte parte 1";
+    let opinion;
+</script>
+<main>
+    <h2>Ingrese una opinión/review de {filmName} </h2>
+    <div class=input>
+        <input bind:value={opinion} class="border border-primary" type="text" size="30" maxlength="500">
+        <button name="send"> Enviar </button>
+    </div>
+</main>
+<style>
+    main {
+        display: flex;
+    }
+
+    .input {
+        display: grid;
+        grid-template-columns: 800px 100px;
+    }
+</style>
