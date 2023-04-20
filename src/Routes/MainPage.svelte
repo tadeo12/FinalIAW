e294cca49346a7bd64d74359e853734d3aabc6a6
Tadeo <tadeoova@gmail.com>
2023-04-20 18:03:04 -0300

    busqueda de peliculas con enter

--- src/Routes/MainPage.svelte
+++ src/Routes/MainPage.svelte
@@ -44,6 +44,7 @@
 	function searchMovies() {
     	buscando=true;
 		movies=[];
+		redirectToMovieSearch();
 		getMovies(filmName)
 		.then((e) => {
 			console.log("data movies", e)
@@ -75,26 +76,31 @@
 
 <main>
 
-	<div class="busqueda grey darken-3  opacity-85">
-		<input class="white-text text-darken-2" placeholder="Pelicula" id="first_name" type="text" bind:value={filmName} on:input={handleInputFilmName} >
-		<button  class="waves-effect waves-light btn grey darken-2" on:click={redirectToMovieSearch} name="action">
-			<i class="material-icons">search</i>
-		</button>
-	</div>
-
+	
+		<form class="busqueda grey darken-3  opacity-85" on:submit|preventDefault={() => searchMovies() }>
+			<label for="">
+				<input class="white-text text-darken-2" placeholder="Pelicula" id="first_name" type="text" bind:value={filmName} on:input={handleInputFilmName} >
+			</label>
+			<button  class="waves-effect waves-light btn grey darken-2" name="action">
+				<i class="material-icons">search</i>
+			</button>
+		</form>
+	
 	<MoviesTable data={cache} movies={movies} filmName={filmName} cargando={buscando}/>
 </main>
 
 <style>
-	main {
-	
-		
-	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 300;
+	.busqueda {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+  	}
+
+	.busqueda label {
+		width: 100%;
+		flex: 1;
+		margin-right: 10px;
 	}
 	
 </style>
